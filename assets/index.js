74e902e368fcd5ca6ce982ff5cf79a92dc203831
Darsh Gupta <darsh.gupta.1602@gmail.com>
2021-06-26 18:41:52 +0530

    Add Styles and Functionality

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,39 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "Hello World!",
+      title: "5 Minute Procrastination Timer",
+      btnValue: "Start",
+      timerDisplay: "00:00",
+      time: 5 * 60,
+      interval: null,
+      i: null,
     };
+  },
+  methods: {
+    btnClicked() {
+      if (this.btnValue === "Start") {
+        this.btnValue = "Cancel";
+        this.i = this.time;
+        let i = this.i;
+        this.interval = setInterval(() => {
+          if (i != 0) {
+            i--;
+            let minutes = ~~(i / 60);
+            let seconds = ~~(i % 60);
+
+            this.timerDisplay =
+              "0" + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+          } else {
+            new Audio("/assets/Finish Signal.mp3").play();
+            this.btnValue = "Start";
+            alert("Switch to a new task");
+            clearInterval(this.interval);
+          }
+        }, 1000);
+      } else {
+        window.location.reload();
+      }
+    },
   },
 });
 
